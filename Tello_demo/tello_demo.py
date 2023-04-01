8aa4a35d27af9ae5cd36e6cba7eb16991acc9db1
C442 <MayCh727@school.lu>
2023-04-01 21:52:46 +0200

    update
    pushing slight changes

--- Tello_demo/tello_demo.py
+++ Tello_demo/tello_demo.py
@@ -4,22 +4,16 @@
 # It is more childfriendly+ can have more commands now(around 500)
 
 # -*- coding: utf-8 -*-
-from cProfile import label
 import csv
 import copy
 import argparse
 import itertools
 import time
 
-import cv2 as cv
-from cv2 import FONT_HERSHEY_COMPLEX
-from cv2 import FILLED
-from cv2 import LINE_AA
-from cv2 import waitKey
+import cv2
 import numpy as np
 import mediapipe as mp
 from djitellopy import tello
-from sklearn.metrics import classification_report
 
 
 from utils import CvFpsCalc
@@ -34,38 +28,38 @@ StaticImgResize = 300
 HandSignList = []
 
 # Getting+resizing respective images to display
-TAKEOFF = cv.imread('Tello_Command_Images/TAKEOFF.png')
-TAKEOFF_RESIZED = cv.resize(TAKEOFF, (StaticImgResize, StaticImgResize))
+TAKEOFF = cv2.imread('Tello_Command_Images/TAKEOFF.png')
+TAKEOFF_RESIZED = cv2.resize(TAKEOFF, (StaticImgResize, StaticImgResize))
 
-LAND = cv.imread('Tello_Command_Images/LAND.png')
-LAND_RESIZED = cv.resize(LAND, (StaticImgResize, StaticImgResize))
+LAND = cv2.imread('Tello_Command_Images/LAND.png')
+LAND_RESIZED = cv2.resize(LAND, (StaticImgResize, StaticImgResize))
 
-LEFT = cv.imread('Tello_Command_Images/LEFT.png')
-LEFT_RESIZED = cv.resize(LEFT, (StaticImgResize, StaticImgResize))
+LEFT = cv2.imread('Tello_Command_Images/LEFT.png')
+LEFT_RESIZED = cv2.resize(LEFT, (StaticImgResize, StaticImgResize))
 
-RIGHT = cv.imread('Tello_Command_Images/RIGHT.png')
-RIGHT_RESIZED = cv.resize(RIGHT, (StaticImgResize, StaticImgResize))
+RIGHT = cv2.imread('Tello_Command_Images/RIGHT.png')
+RIGHT_RESIZED = cv2.resize(RIGHT, (StaticImgResize, StaticImgResize))
 
-BACKWARDS = cv.imread('Tello_Command_Images/BACKWARDS.jpg')
-BACKWARDS_RESIZED = cv.resize(BACKWARDS, (StaticImgResize, StaticImgResize))
+BACKWARDS = cv2.imread('Tello_Command_Images/BACKWARDS.jpg')
+BACKWARDS_RESIZED = cv2.resize(BACKWARDS, (StaticImgResize, StaticImgResize))
 
-FORWARDS = cv.imread('Tello_Command_Images/FORWARDS.jpg')
-FORWARDS_RESIZED = cv.resize(FORWARDS, (StaticImgResize, StaticImgResize))
+FORWARDS = cv2.imread('Tello_Command_Images/FORWARDS.jpg')
+FORWARDS_RESIZED = cv2.resize(FORWARDS, (StaticImgResize, StaticImgResize))
 
-UP = cv.imread('Tello_Command_Images/UP.png')
-UP_RESIZED = cv.resize(UP, (StaticImgResize, StaticImgResize))
+UP = cv2.imread('Tello_Command_Images/UP.png')
+UP_RESIZED = cv2.resize(UP, (StaticImgResize, StaticImgResize))
 
-DOWN = cv.imread('Tello_Command_Images/DOWN.png')
-DOWN_RESIZED = cv.resize(DOWN, (StaticImgResize, StaticImgResize))
+DOWN = cv2.imread('Tello_Command_Images/DOWN.png')
+DOWN_RESIZED = cv2.resize(DOWN, (StaticImgResize, StaticImgResize))
 
-YAWcw = cv.imread('Tello_Command_Images/YAWcw.jpeg')
-YAWcw_RESIZED = cv.resize(YAWcw, (StaticImgResize, StaticImgResize))
+YAWcw = cv2.imread('Tello_Command_Images/YAWcw.jpeg')
+YAWcw_RESIZED = cv2.resize(YAWcw, (StaticImgResize, StaticImgResize))
 
-YAWccw = cv.imread('Tello_Command_Images/YAWccw.jpeg')
-YAWccw_RESIZED = cv.resize(YAWccw, (StaticImgResize, StaticImgResize))
+YAWccw = cv2.imread('Tello_Command_Images/YAWccw.jpeg')
+YAWccw_RESIZED = cv2.resize(YAWccw, (StaticImgResize, StaticImgResize))
 
-FLIP = cv.imread('Tello_Command_Images/FLIP.jpeg')
-FLIP_RESIZED = cv.resize(FLIP, (StaticImgResize, StaticImgResize))
+FLIP = cv2.imread('Tello_Command_Images/FLIP.jpeg')
+FLIP_RESIZED = cv2.resize(FLIP, (StaticImgResize, StaticImgResize))
 
 
 def get_args():
@@ -106,13 +100,12 @@ def main():
     Cnt = 0
     while True:
         fps = cvFpsCalc.get()
-        key = cv.waitKey(1)
+        key = cv2.waitKey(1)
         if key == ord("q"):
             Drone.land()
             Drone.streamoff()
-            cv.destroyAllWindows()
+            cv2.destroyAllWindows()
             break
-        
 
         # success, image = cap.read()
 
@@ -122,10 +115,10 @@ def main():
         if image is None:
             continue
 
-        image = cv.flip(image, 1)  # Mirror display
+        image = cv2.flip(image, 1)  # Mirror display
         debug_image = copy.deepcopy(image)
         # Detection implementation
-        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
         results = hands.process(image)
         image.flags.writeable = True
@@ -161,14 +154,14 @@ def main():
 
             if Cnt >= 50:
                 print(f" Battery : {Drone.get_battery()}")
-                cv.destroyWindow("Order")
+                cv2.destroyWindow("Order")
                 flight_evaluation(correctly_formated_handsign_list)
                 print(f"{correctly_formated_handsign_list}\n")
                 Cnt = 0
                 current_list = ["", ""]
 
         debug_image = draw_info(debug_image, fps, mode, number)
-        cv.imshow('Hand Gesture Recognition', debug_image)
+        cv2.imshow('Hand Gesture Recognition', debug_image)
 
         # keyboard control
         keyboard_control()
@@ -176,7 +169,7 @@ def main():
 
 def keyboard_control():
     # (q)uit and (t)ake off for quit and start
-    key = waitKey(1)
+    key = cv2.waitKey(1)
     if not Drone.is_flying:
         if key == ord("t"):
             Drone.takeoff()
@@ -238,7 +231,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 2_takeoff
             case['6L', '6R'] | ['', '1R']:
                 print("takeoff")
-                cv.imshow("Order", TAKEOFF_RESIZED)
+                cv2.imshow("Order", TAKEOFF_RESIZED)
                 Drone.takeoff()
                 time.sleep(0.5)
 
@@ -247,13 +240,13 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 1_land
             case['8L', '5R']:
                 print("land")
-                cv.imshow("Order", LAND_RESIZED)
+                cv2.imshow("Order", LAND_RESIZED)
                 Drone.land()
 
             # 3_right
             case['0L', '1R']:
                 print("Right")
-                cv.imshow("Order", RIGHT_RESIZED)
+                cv2.imshow("Order", RIGHT_RESIZED)
                 Drone.send_rc_control(-100, 0, 0, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -261,7 +254,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 4_left
             case['0L', '2R']:
                 print("Left")
-                cv.imshow("Order", LEFT_RESIZED)
+                cv2.imshow("Order", LEFT_RESIZED)
                 Drone.send_rc_control(100, 0, 0, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -269,7 +262,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 5_up
             case['1L', '6R']:
                 print("Up")
-                cv.imshow("Order", UP_RESIZED)
+                cv2.imshow("Order", UP_RESIZED)
                 Drone.send_rc_control(0, 0, 100, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -277,7 +270,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 6_down
             case['1L', '7R']:
                 print("Down")
-                cv.imshow("Order", DOWN_RESIZED)
+                cv2.imshow("Order", DOWN_RESIZED)
                 Drone.send_rc_control(0, 0, -100, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -285,7 +278,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 7_forwards
             case['0L', '4R']:
                 print("forwards")
-                cv.imshow("Order", FORWARDS_RESIZED)
+                cv2.imshow("Order", FORWARDS_RESIZED)
                 Drone.send_rc_control(0, 100, 0, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -293,7 +286,7 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 8_backwards
             case['0L', '5R']:
                 print("backwards")
-                cv.imshow("Order", BACKWARDS_RESIZED)
+                cv2.imshow("Order", BACKWARDS_RESIZED)
                 Drone.send_rc_control(0, -100, 0, 0)
                 time.sleep(0.5)
                 Drone.send_rc_control(0, 0, 0, 0)
@@ -301,21 +294,21 @@ def flight_evaluation(correctly_formated_handsign_list):
             # 9_flip(a)
             case['0L', '3R']:
                 print("flip")
-                cv.imshow("Order", FLIP_RESIZED)
+                cv2.imshow("Order", FLIP_RESIZED)
                 Drone.flip_back()
                 time.sleep(0.5)
 
             # 10_flip(b)
             case['6L', '3R']:
                 print("flip")
-                cv.imshow("Order", FLIP_RESIZED)
+                cv2.imshow("Order", FLIP_RESIZED)
                 Drone.flip_forward()
                 time.sleep(0.5)
 
             # 11 FLIP MAYHEM
             case['8L', '2R']:
                 print("flip")
-                cv.imshow("Order", FLIP_RESIZED)
+                cv2.imshow("Order", FLIP_RESIZED)
                 Drone.flip_forward()
                 time.sleep(0.5)
                 Drone.flip_right()
@@ -340,7 +333,7 @@ def calc_bounding_rect(image, landmarks):
 
         landmark_array = np.append(landmark_array, landmark_point, axis=0)
 
-    x, y, w, h = cv.boundingRect(landmark_array)
+    x, y, w, h = cv2.boundingRect(landmark_array)
 
     return [x, y, x + w, y + h]
 
@@ -391,115 +384,115 @@ def pre_process_landmark(landmark_list):
 def draw_landmarks(image, landmark_point):
     if len(landmark_point) > 0:
         # Thumb
-        cv.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
-                (255, 255, 255), 6)
-        cv.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
-                (255, 255, 255), 6)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[3]),
+                 (255, 255, 255), 6)
+        cv2.line(image, tuple(landmark_point[3]), tuple(landmark_point[4]),
+                 (255, 255, 255), 6)
 
         # Index finger
 
-        cv.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[6]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[6]), tuple(landmark_point[7]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[7]), tuple(landmark_point[8]),
+                 (255, 255, 255), 4)
 
         # Middle finger
-        cv.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[10]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[10]), tuple(landmark_point[11]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[11]), tuple(landmark_point[12]),
+                 (255, 255, 255), 4)
 
         # Ring finger
-        cv.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[14]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[14]), tuple(landmark_point[15]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[15]), tuple(landmark_point[16]),
+                 (255, 255, 255), 4)
 
         # Little finger
 
-        cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[18]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[18]), tuple(landmark_point[19]),
+                 (255, 255, 255), 4)
 
-        cv.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
-                (255, 255, 255), 4)
+        cv2.line(image, tuple(landmark_point[19]), tuple(landmark_point[20]),
+                 (255, 255, 255), 4)
 
         # Palm
 
-        cv.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[0]), tuple(landmark_point[1]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[1]), tuple(landmark_point[2]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[2]), tuple(landmark_point[5]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[5]), tuple(landmark_point[9]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[9]), tuple(landmark_point[13]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[13]), tuple(landmark_point[17]),
+                 (255, 255, 255), 5)
 
-        cv.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
-                (255, 255, 255), 5)
+        cv2.line(image, tuple(landmark_point[17]), tuple(landmark_point[0]),
+                 (255, 255, 255), 5)
 
     # Key Points
     for index, landmark in enumerate(landmark_point):
         if index % 4 == 0 and index > 0:
-            cv.circle(image, (landmark[0], landmark[1]), 8, (204, 0, 0), -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 8, (204, 0, 0), -1)
         else:
-            cv.circle(image, (landmark[0], landmark[1]), 5, (204, 0, 0), -1)
+            cv2.circle(image, (landmark[0], landmark[1]), 5, (204, 0, 0), -1)
     return image
 
 
 def draw_bounding_rect(use_brect, image, brect):
     if use_brect:
         # Outer rectangle
-        cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
-                     (255, 20, 147), 4)
+        cv2.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
+                      (255, 20, 147), 4)
 
     return image
 
 
 def draw_info_text(image, brect, handedness, hand_sign_text,
                    finger_gesture_text):
-    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
-                 (0, 0, 0), -1)
+    cv2.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
+                  (0, 0, 0), -1)
     info_text = handedness.classification[0].label[0:]
     if hand_sign_text != "":
         info_text = info_text + ': ' + hand_sign_text
-    cv.putText(image, str(info_text), (brect[0] + 5, brect[1] - 4),
-               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2, cv.LINE_AA)
+    cv2.putText(image, str(info_text), (brect[0] + 5, brect[1] - 4),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2, cv2.LINE_AA)
 
     if finger_gesture_text != "":
-        cv.putText(image, info_text, (10, 350),
-                   cv.FONT_HERSHEY_SIMPLEX, 1.0, (127, 0, 255), 2,
-                   cv.LINE_AA)
+        cv2.putText(image, info_text, (10, 350),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (127, 0, 255), 2,
+                    cv2.LINE_AA)
 
     return image
 
 
 def draw_info(image, fps, mode, number):
-    cv.putText(image, "FPS:" + str(fps), (10, 390), cv.FONT_HERSHEY_SIMPLEX,
-               1.0, (0, 255, 0), 2, cv.LINE_AA)
+    cv2.putText(image, "FPS:" + str(fps), (10, 390), cv2.FONT_HERSHEY_SIMPLEX,
+                1.0, (0, 255, 0), 2, cv2.LINE_AA)
     return image
 
 
